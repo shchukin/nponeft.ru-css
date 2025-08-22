46ebb2528d448f3873be3ea4ab1cef420662c19b
Anton Shchukin <a.a.shchukin@gmail.com>
2025-08-22 08:31:12 +0700

    scripts

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,57 @@
 (function($) {
 
+    /* Инициализация модалок */
+
+    $('.mfp-handler').magnificPopup({
+        type: 'inline',
+        removalDelay: 200,
+        showCloseBtn: false
+    });
+
+
+    /* Инпуты */
+
+
+    /* placeholder в <select> */
+
+    function selectPlaceholder($element) {
+        if ($element.val() === 'placeholder') {
+            $element.parent('.input').addClass('input--placeholder-is-chosen');
+        } else {
+            $element.parent('.input').removeClass('input--placeholder-is-chosen');
+        }
+    }
+
+    $('select.input__widget').each(function () {
+        selectPlaceholder($(this));
+    }).on('change', function () {
+        selectPlaceholder($(this));
+    });
+
+
+    /* input--expandable */
+    function expandTextarea($element) {
+        $element.css('height', 'auto');
+        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+    }
+
+    $('.input--expandable .input__widget').each(function () {
+        expandTextarea($(this));
+    }).on('input', function () {
+        expandTextarea($(this));
+    });
+
+
+    /* input--error */
+    $('.input__widget').on('focus', function () {
+        $(this).parents('.input').removeClass('input--error');
+        $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+    });
+
+
+
+    /* Табы */
+
     /* Используем методы siblings и children чтобы не искать вглубь, и не натолкнуться на вложенные табы */
     $('.tabs__tag').click(function() {
 
