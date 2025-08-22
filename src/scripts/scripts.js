189162c82230f5efb6be8c3907e432f158320e58
Anton Shchukin <a.a.shchukin@gmail.com>
2025-08-22 15:50:45 +0700

    burger

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,22 @@
 (function($) {
 
+    /* Бургер */
+
+    const $html = $('html');
+    let rememberedPageScrollPosition = 0;
+
+    $('.header__burger').on('click', function () {
+
+        if( ! $html.hasClass('burger-expanded') ) {
+            rememberedPageScrollPosition = $(window).scrollTop(); /* Запомнить скролл пользователя, так как display: none на .page его сбросит (смотри .burger-expanded .page) */
+            $html.addClass('burger-expanded');
+            $(window).scrollTop(0); /* При открытии меню его скролл должен быть в начале */
+        } else {
+            $html.removeClass('burger-expanded');
+            $(window).scrollTop(rememberedPageScrollPosition);/* При закрытии меню скролл должен быть там, где пользователь его оставил */
+        }
+    });
+
     /* Инициализация модалок */
 
     $('.mfp-handler').magnificPopup({
